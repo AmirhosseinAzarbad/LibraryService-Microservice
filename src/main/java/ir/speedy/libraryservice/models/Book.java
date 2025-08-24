91a984e9590a7c946c21269adc2d2244a71720fc
speedy <thenewamirech@gmail.com>
2025-08-24 08:37:25 +0330

    Added Book entity

---
+++ src/main/java/ir/speedy/libraryservice/models/Book.java
@@ -0,0 +1,21 @@
+package ir.speedy.libraryservice.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "bookTable")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String author;
+    private int price;
+}
